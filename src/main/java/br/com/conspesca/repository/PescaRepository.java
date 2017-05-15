bbb55b877b9cd8d322463b669a5ab7b31f1bc8fa
Natan da Silva <natanavnet@tractebelenergia.com.br>
2017-05-15 20:19:25 -0300

    Ajustes nos projetos

--- src/main/java/br/com/conspesca/repository/PescaRepository.java
+++ src/main/java/br/com/conspesca/repository/PescaRepository.java
@@ -5,7 +5,9 @@ import java.util.List;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 import br.com.conspesca.model.Pesca;
 
@@ -29,8 +31,16 @@ public class PescaRepository {
 	}
 
 	public List<Pesca> findAll() {
-		CriteriaQuery<Pesca> cq = this.em.getCriteriaBuilder().createQuery(Pesca.class);
-		cq.select(cq.from(Pesca.class));
+		
+		CriteriaBuilder builder = this.em.getCriteriaBuilder();
+		CriteriaQuery<Pesca> cq = builder.createQuery(Pesca.class);
+		Root<Pesca> rootP = cq.from(Pesca.class);
+		rootP.fetch("pescarias");
+		cq.select(rootP);
+		
+		cq.distinct(true);
+		
+		
 		return this.em.createQuery(cq).getResultList();
 	}
 
